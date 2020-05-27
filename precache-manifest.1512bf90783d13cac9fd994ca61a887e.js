b879716044d314669d6bc5b0780d61142aadba39
Arun Kumar Selvaraj <dev@ioak.org>
2020-05-27 20:03:05 +0530

    Updates

---
+++ precache-manifest.1512bf90783d13cac9fd994ca61a887e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f82d8caafde74cd6b6aa873b16fcbbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e07891eb3fdd3a6d3e7f",
+    "url": "/static/css/main.50bd7beb.chunk.css"
+  },
+  {
+    "revision": "9b04a33d694a7c6d299a",
+    "url": "/static/js/2.144a3224.chunk.js"
+  },
+  {
+    "revision": "e89ca22e023f5a385d87cd8aba699af6",
+    "url": "/static/js/2.144a3224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07891eb3fdd3a6d3e7f",
+    "url": "/static/js/main.45ce8820.chunk.js"
+  },
+  {
+    "revision": "6d8ca3f47cffa3481796",
+    "url": "/static/js/runtime-main.5b923dbb.js"
+  },
+  {
+    "revision": "bc9580be27db18160cd8075f5679f0d3",
+    "url": "/static/media/login_bg.bc9580be.jpg"
+  },
+  {
+    "revision": "9102694daa5be2042b929b143327b65d",
+    "url": "/static/media/reshape_black.9102694d.svg"
+  },
+  {
+    "revision": "d9be0934612ad9cd90f2e0506a22e549",
+    "url": "/static/media/reshape_white.d9be0934.svg"
+  },
+  {
+    "revision": "7aed2e3864522998087fa67b1c5435ce",
+    "url": "/static/media/reshape_white_small.7aed2e38.svg"
+  }
+]);
